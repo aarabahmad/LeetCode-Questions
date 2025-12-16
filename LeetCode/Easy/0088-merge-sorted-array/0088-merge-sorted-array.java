4880d0adb12856421a11ee64ee3c7fb0d8f7b9af
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2025-12-16 16:22:07 +0530

    0 ms (100%) 43.7 MB (39.84%) 12-16-2025 0088-merge-sorted-array Easy .java

--- LeetCode/Easy/0088-merge-sorted-array/0088-merge-sorted-array.java
+++ LeetCode/Easy/0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -10,24 +10,21 @@ class Solution {
             return;
         }
         else {
-            // for(int i=m;i<m+n;i++) {
-            //     nums1[i]=nums2[i-m];
-            // }
             int i=m-1;
             int j=n-1;
-            int k=m+n-1;
+            int index=m+n-1;
             while(i>=0 && j>=0) {
                 if(nums1[i]>nums2[j]) {
-                    nums1[k--]=nums1[i--];
+                    nums1[index--]=nums1[i--];
                 }
                 else {
-                    nums1[k--]=nums2[j--];
+                    nums1[index--]=nums2[j--];
                 }
             }
             while(j>=0) {
-                nums1[k--]=nums2[j--];
+                nums1[index--]=nums2[j--];
             }
-            
+            return;
         }
     }
 }
