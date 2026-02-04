e4e9df2ea45a67ea2cd4254a24e1d86ebc65f708
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-02-04 12:20:47 +0530

    2 ms (99.41%) 44.2 MB (88.87%) 02-04-2026 0242-valid-anagram Easy .java

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
@@ -3,12 +3,16 @@ class Solution {
         if(s.length()!=t.length()) {
             return false;
         }
-        char[] sArr=s.toCharArray();
-        char[] tArr=t.toCharArray();
-        Arrays.sort(sArr);
-        Arrays.sort(tArr);
-        for(int i=0;i<sArr.length;i++) {
-            if(sArr[i]!=tArr[i]) {
+        char[] alpha=new char[26];
+        for(char ch : s.toCharArray()) {
+            alpha[ch-'a']++;
+        }
+        for(char ch : t.toCharArray()) {
+            alpha[ch-'a']--;
+        }
+        int count=0;
+        for(int i=0;i<alpha.length;i++) {
+            if(alpha[i]!=0) {
                 return false;
             }
         }
