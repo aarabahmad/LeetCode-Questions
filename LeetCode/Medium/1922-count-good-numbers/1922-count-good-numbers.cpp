0b95e53b6f5570c9af0fcc06d086dfaf89c916dd
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-03-03 18:25:47 +0530

    0 ms (100%) 7.9 MB (35.89%) 03-03-2026 1922-count-good-numbers Medium .cpp

---
+++ LeetCode/Medium/1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    const int  MOD=1e9+7;
+    
+    long long p(long long a, long long b){
+        if(b==0){
+            return 1;
+        }
+
+        long long half=p(a,b/2);
+        long long ans=(half*half)%MOD;
+
+        if(b%2==1){
+            ans=(ans*a)%MOD;
+        }
+        return ans;
+    }
+    int countGoodNumbers(long long n) {
+        return p(5,(n+1)/2)* p(4,n/2)%MOD;
+
+        
+    }
+};
