6be055b51529b8ebcd7bb74d5aa4f372c496d95a
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-03-03 16:43:20 +0530

    149 ms (42.2%) 42.8 MB (89.09%) 03-03-2026 0079-word-search Medium .java

---
+++ LeetCode/Medium/0079-word-search/0079-word-search.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                boolean[][] visited = new boolean[board.length][board[0].length];
+                if (isValid(board, word, visited, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean isValid(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
+        if(index==word.length()) {
+            return true;
+        }
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length) {
+            return false;
+        }
+        if(visited[i][j] || board[i][j]!=word.charAt(index)) {
+            return false;
+        }
+        visited[i][j]=true;
+        if(
+        isValid(board, word, visited, i+1, j, index+1) || 
+        isValid(board, word, visited, i-1, j, index+1) || 
+        isValid(board, word, visited, i, j+1, index+1) || 
+        isValid(board, word, visited, i, j-1, index+1)) {
+            return true;
+        }
+        visited[i][j]=false;
+        return false;
+    }
+}
