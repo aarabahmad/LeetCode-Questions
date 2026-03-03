8516b0960aa058f10628a7ec84615b6e9ec2a41e
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-03-03 17:53:04 +0530

    102 ms (16.35%) 98.5 MB (5.05%) 03-03-2026 1545-find-kth-bit-in-nth-binary-string Medium .java

---
+++ LeetCode/Medium/1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.java
@@ -0,0 +1,30 @@
+class Solution {
+    public char findKthBit(int n, int k) {
+        StringBuilder result=find(n, k, new StringBuilder(), 0);
+        return result.toString().charAt(k-1);
+    }
+
+    public StringBuilder inversion(StringBuilder res) {
+        String invert=res.toString();
+        invert=invert.replace('0', '-');
+        invert=invert.replace('1', '0');
+        invert=invert.replace('-', '1');
+        return new StringBuilder(invert);
+    }
+
+    public StringBuilder find(int n, int k, StringBuilder res, int index) {
+        if(index>n) {
+            return res;
+        }
+        if(index==0) {
+            res.append(0);
+        }
+        else {
+            StringBuilder invert=inversion(res);
+            res.append(1);
+            res.append(invert.reverse());
+            // System.out.println(res);
+        }
+        return find(n, k, res, index+1);
+    }
+}
