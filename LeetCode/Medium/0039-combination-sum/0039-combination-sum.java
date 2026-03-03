ae4f1688c98afc33a43285156a24ee3c64de5a9a
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-03-03 15:42:33 +0530

    N/A (0%) N/A (0%) 03-03-2026 0039-combination-sum Medium .java

--- LeetCode/Medium/0039-combination-sum/0039-combination-sum.java
+++ LeetCode/Medium/0039-combination-sum/0039-combination-sum.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result=new ArrayList<>();
-        Arrays.sort(candidates);
+        // Arrays.sort(candidates);
         backtrack(result, new ArrayList<>(), candidates, target, 0);
         return result;
     }
