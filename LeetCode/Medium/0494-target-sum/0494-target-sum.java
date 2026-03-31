92b69037432c6e5581a19615316391208a99fc5f
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-03-31 13:53:34 +0530

    631 ms (15.79%) 43.3 MB (63.14%) 03-31-2026 0494-target-sum Medium .java

--- LeetCode/Medium/0494-target-sum/0494-target-sum.java
+++ LeetCode/Medium/0494-target-sum/0494-target-sum.java
@@ -1,18 +1,17 @@
 class Solution {
     int count=0;
     public int findTargetSumWays(int[] nums, int target) {
-        findCount(nums, target, 0, 0);
+        findWays(nums, target, 0, 0);
         return count;
     }
-
-    public void findCount(int[] nums, int target, int sum, int index) {
+    public void findWays(int[] nums, int target, int sum, int index) {
         if(index==nums.length) {
             if(sum==target) {
                 count++;
             }
             return;
         }
-        findCount(nums, target, sum+nums[index], index+1);
-        findCount(nums, target, sum-nums[index], index+1);
+        findWays(nums, target, sum+nums[index], index+1);
+        findWays(nums, target, sum-nums[index], index+1);
     }
 }
