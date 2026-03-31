4908811fba630e2b1827d85cd4c17435a6c80a64
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-03-31 13:48:45 +0530

    N/A (0%) N/A (0%) 03-31-2026 0079-word-search Medium .java

--- LeetCode/Medium/0079-word-search/0079-word-search.java
+++ LeetCode/Medium/0079-word-search/0079-word-search.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                boolean[][] visited = new boolean[board.length][board[0].length];
-                if (isValid(board, word, visited, i, j, 0)) {
+        for(int i=0;i<board.length;i++) {
+            for(int j=0;j<board[0].length;j++) {
+                boolean visited[][]=new boolean[board.length][board[0].length];
+                if(isValid(board, word, visited, i, j, 0)) {
                     return true;
                 }
             }
@@ -18,15 +18,16 @@ class Solution {
         if(i<0 || i>=board.length || j<0 || j>=board[0].length) {
             return false;
         }
-        if(visited[i][j] || board[i][j]!=word.charAt(index)) {
+        if(visited[i][j] || word.charAt(index)!=board[i][j]) {
             return false;
         }
         visited[i][j]=true;
         if(
-        isValid(board, word, visited, i+1, j, index+1) || 
-        isValid(board, word, visited, i-1, j, index+1) || 
-        isValid(board, word, visited, i, j+1, index+1) || 
-        isValid(board, word, visited, i, j-1, index+1)) {
+        isValid(board, word, visited, i+1, j, index+1) ||
+        isValid(board, word, visited, i-1, j, index+1) ||
+        isValid(board, word, visited, i, j+1, index+1) ||
+        isValid(board, word, visited, i, j-1, index+1)) 
+        {
             return true;
         }
         visited[i][j]=false;
