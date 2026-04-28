7d69d48b7309cc97bd985864527b2d7427def197
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-04-28 11:12:38 +0530

    2 ms (77.48%) 103.8 MB (93.21%) 04-28-2026 3909-compare-sums-of-bitonic-parts Medium .java

---
+++ LeetCode/Medium/3909-compare-sums-of-bitonic-parts/3909-compare-sums-of-bitonic-parts.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int compareBitonicSums(int[] nums) {
+        int peak=0;
+        for(int i=0;i<nums.length-1;i++) {
+            if(nums[i]>nums[i+1]) {
+                peak=i;
+                break;
+            }
+        }
+        // System.out.println(peak);
+        long incSum=0, decSum=0;
+        for(int i=0;i<=peak;i++) {
+            incSum+=nums[i];
+        }
+        for(int i=peak;i<nums.length;i++) {
+            decSum+=nums[i];
+        }
+        if(incSum>decSum) {
+            return 0;
+        }
+        else if(decSum>incSum) {
+            return 1;
+        }
+        return -1;
+    }
+}
