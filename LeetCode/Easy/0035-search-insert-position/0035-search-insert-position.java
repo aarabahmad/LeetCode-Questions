7d1c5dce0d89ba1f4c111887d52eeac049489c78
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-05-07 13:57:32 +0530

    0 ms (100%) 44.8 MB (47.67%) 05-07-2026 0035-search-insert-position Easy .java

--- LeetCode/Easy/0035-search-insert-position/0035-search-insert-position.java
+++ LeetCode/Easy/0035-search-insert-position/0035-search-insert-position.java
@@ -1,26 +1,25 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int start=0, end=nums.length-1;
-        if(target<nums[0]) {
-            return 0;
+        int ans=-1;
+        if(target<nums[start]) {
+            return start;
         }
         else if(target>nums[end]) {
             return end+1;
         }
         else {
-            while(start<end) {
+            while(start<=end) {
                 int mid=start+(end-start)/2;
-                if(nums[mid]==target) {
-                    return mid;
-                }
-                else if(nums[mid]>target) {
-                    end=mid;
+                if(nums[mid]>=target) {
+                    ans=mid;
+                    end=mid-1;
                 }
                 else {
                     start=mid+1;
                 }
             }
+            return ans;
         }
-        return start;
     }
 }
