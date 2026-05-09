5c25123ae7f5addaf139d5fed697dc8e61dd19e4
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-05-09 16:18:08 +0530

    12 ms (54.58%) 48 MB (29.44%) 05-09-2026 0875-koko-eating-bananas Medium .java

--- LeetCode/Medium/0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ LeetCode/Medium/0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,28 +1,27 @@
 class Solution {
-    public int minEatingSpeed(int[] piles, int hour) {
-        int start=1, end=0;
+    public int minEatingSpeed(int[] piles, int h) {
+        int start=1, end=Integer.MIN_VALUE, res=0;
         for(int i : piles) {
-            end=Math.max(end, i);
+            end=Math.max(i, end);
         }
-        int result=0;
         while(start<=end) {
             int mid=start+(end-start)/2;
-            if(canEatAll(piles, mid, hour)) {
-                result=mid;
+            if(canEatAll(piles, h, mid)) {
+                res=mid;
                 end=mid-1;
             }
             else {
                 start=mid+1;
             }
         }
-        return result;
+        return res;
     }
 
-    public boolean canEatAll(int[] piles, int rate, int hours) {
+    public static boolean canEatAll(int[] piles, int hours, int rate) {
         int hour=0;
-        for(int i=0;i<piles.length;i++) {
-            hour+=(piles[i]/rate);
-            if(piles[i]%rate!=0) {
+        for(int i : piles) {
+            hour+=(i/rate);
+            if(i%rate!=0) {
                 hour++;
             }
             if(hour>hours) {
