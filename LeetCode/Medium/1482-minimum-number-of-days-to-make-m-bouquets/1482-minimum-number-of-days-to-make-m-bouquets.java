25379e437f1c3612dd2a2af36d898a0530d078ec
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-05-09 16:52:06 +0530

    14 ms (98.02%) 82.5 MB (65.57%) 05-09-2026 1482-minimum-number-of-days-to-make-m-bouquets Medium .java

---
+++ LeetCode/Medium/1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+        if((m*k)>bloomDay.length) {
+            return -1;
+        }
+        int start=1, end=0, res=-1;
+        for(int i : bloomDay) {
+            end=Math.max(i, end);
+        }
+        while(start<=end) {
+            int mid=start+(end-start)/2;
+            if(canMakeBouquets(bloomDay, m, k, mid)) {
+                res=mid;
+                end=mid-1;
+            }
+            else {
+                start=mid+1;
+            }
+        }
+        return res;
+    }
+
+    public boolean canMakeBouquets(int[] bloomDay, int bouquets, int adjacent, int days) {
+        int total=0;
+        for(int i=0;i<bloomDay.length;i++) {
+            int count=0;
+            while(i<bloomDay.length && count<adjacent && bloomDay[i]<=days) {
+                count++;
+                i++;
+            }
+            if(count==adjacent) {
+                total++;
+                i--;
+            }
+            if(total>=bouquets) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
