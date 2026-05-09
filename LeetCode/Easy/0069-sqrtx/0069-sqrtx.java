addfc7f66367d752c69af2ef85aa6b9b1caffee8
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-05-09 14:43:42 +0530

    1 ms (97.93%) 42.3 MB (94.59%) 05-09-2026 0069-sqrtx Easy .java

--- LeetCode/Easy/0069-sqrtx/0069-sqrtx.java
+++ LeetCode/Easy/0069-sqrtx/0069-sqrtx.java
@@ -1,22 +1,20 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x < 2) {
+        if(x<2) {
             return x;
         }
-        int left = 1, right = x / 2, result = 0;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (mid == x / mid) {
-                return mid;
-            } 
-            else if (mid < x / mid) {
-                result = mid;
-                left = mid + 1;
-            } 
+        int start=1, end=x/2;
+        int ans=-1;
+        while(start<=end) {
+            int mid=start+(end-start)/2;
+            if(mid>x/mid) {
+                end=mid-1;
+            }
             else {
-                right = mid - 1;
+                ans=mid;
+                start=mid+1;
             }
         }
-        return result;
+        return ans;
     }
 }
