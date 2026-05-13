130ebfd711f7560c2e8c1e8909f04d99557727fc
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-05-13 17:18:07 +0530

    8 ms (5%) 43.4 MB (7.76%) 05-13-2026 0410-split-array-largest-sum Hard .java

---
+++ LeetCode/Hard/0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int start=nums[0], end=0;
+        for(int num : nums) {
+            start=Math.max(num, start);
+            end+=num;
+        }
+        int res=-1;
+        while(start<=end) {
+            int mid=start+(end-start)/2;
+            System.out.println(start+" "+mid+" "+end);
+            if(canSplit(nums, mid, k)) {
+                res=mid;
+                end=mid-1;
+            }
+            else {
+                start=mid+1;
+            }
+        }
+        return res;
+    }
+
+    public boolean canSplit(int[] nums, int subSum, int subarrays) {
+        int count = 1;
+        int currentSum = 0;
+        for (int num : nums) {
+            currentSum += num;
+            if (currentSum > subSum) {
+                count++;
+                currentSum = num;
+                if (count > subarrays) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
