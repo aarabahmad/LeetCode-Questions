5df06acc0f2d4240f0aceac2ebfda80a1fe77958
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-05-19 16:04:33 +0530

    1 ms (99.96%) 77.3 MB (31.91%) 05-19-2026 0053-maximum-subarray Medium .java

--- LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.java
+++ LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,9 +1,9 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int max=0, res=Integer.MIN_VALUE;
-        for(int i : nums) {
-            max=Math.max(i, max+i);
-            res=Math.max(max, res);
+        int max=nums[0], res=nums[0];
+        for(int i=1;i<nums.length;i++) {
+            max=Math.max(nums[i], max+nums[i]);
+            res=Math.max(res, max);
         }
         return res;
     }
