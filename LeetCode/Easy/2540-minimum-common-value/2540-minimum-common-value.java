217e56d8f2b32a8430f79b2e5e9a181879a84d38
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-05-19 16:25:47 +0530

    118 ms (5.79%) 110.3 MB (5.73%) 05-19-2026 2540-minimum-common-value Easy .java

---
+++ LeetCode/Easy/2540-minimum-common-value/2540-minimum-common-value.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map1=new LinkedHashMap<>();
+        for(int num : nums1) {
+            map1.put(num, map1.getOrDefault(num, 0)+1);
+        }
+        Map<Integer, Integer> map2=new LinkedHashMap<>();
+        for(int num : nums2) {
+            map2.put(num, map2.getOrDefault(num, 0)+1);
+        }
+        System.out.println(map1);
+        System.out.println(map2);
+        for(Map.Entry<Integer, Integer> entry : map1.entrySet()) {
+            if(map2.containsKey(entry.getKey())) {
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+}
