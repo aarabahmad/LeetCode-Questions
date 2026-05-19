e6435eca520f7074f9bb7d5a075546b48b535bc9
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-05-19 14:23:12 +0530

    0 ms (100%) 61.4 MB (29.18%) 05-19-2026 0189-rotate-array Medium .java

--- LeetCode/Medium/0189-rotate-array/0189-rotate-array.java
+++ LeetCode/Medium/0189-rotate-array/0189-rotate-array.java
@@ -1,4 +1,5 @@
 class Solution {
+
     public void reverse(int[] nums, int start, int end) {
         while(start<end) {
             int temp=nums[start];
@@ -8,10 +9,9 @@ class Solution {
             end--;
         }
     }
+
     public void rotate(int[] nums, int k) {
-        while(k>=nums.length) {
-            k=k-nums.length;
-        }
+        k=k%nums.length;
         reverse(nums, 0, nums.length-1);
         reverse(nums, 0, k-1);
         reverse(nums, k, nums.length-1);
