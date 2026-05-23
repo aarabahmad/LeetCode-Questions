9c38b38c16534ae44fd44515b1dd3412095b2542
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-05-23 12:13:42 +0530

    0 ms (100%) 48.2 MB (30.17%) 05-23-2026 0034-find-first-and-last-position-of-element-in-sorted-array Medium .java

--- LeetCode/Medium/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ LeetCode/Medium/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,46 +1,42 @@
 class Solution {
-
-    public static int firstPosition(int[] nums, int target) {
+    public int firstPos(int[] nums, int target) {
         int start=0, end=nums.length-1;
-        int res=-1;
+        int result=-1;
         while(start<=end) {
             int mid=start+(end-start)/2;
-            if(nums[mid]==target) {
-                res=mid;
-                end=mid-1;
-            }
-            else if(nums[mid]>target) {
+            if(nums[mid]>=target) {
+                if(nums[mid]==target) {
+                    result=mid;
+                }
                 end=mid-1;
             }
             else {
                 start=mid+1;
             }
         }
-        return res;
+        return result;
     }
-
-    public static int lastPosition(int[] nums, int target) {
+    public int lastPos(int[] nums, int target) {
         int start=0, end=nums.length-1;
-        int res=-1;
+        int result=-1;
         while(start<=end) {
             int mid=start+(end-start)/2;
-            if(nums[mid]==target) {
-                res=mid;
-                start=mid+1;
-            }
-            else if(nums[mid]<target) {
+            if(nums[mid]<=target) {
+                if(nums[mid]==target) {
+                    result=mid;
+                }
                 start=mid+1;
             }
             else {
                 end=mid-1;
             }
         }
-        return res;
+        return result;
     }
-
     public int[] searchRange(int[] nums, int target) {
-        int first=firstPosition(nums, target);
-        int second=lastPosition(nums, target);
-        return new int[]{first, second};
+        int start=0, end=nums.length-1;
+        int first=firstPos(nums, target);
+        int last=lastPos(nums, target);
+        return new int[]{first, last};
     }
 }
