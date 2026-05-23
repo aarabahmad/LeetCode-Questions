96cda1afc6b90a45082f16ef22701b37cc6dcacb
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-05-23 11:10:52 +0530

    0 ms (100%) 48.1 MB (83.21%) 05-23-2026 0704-binary-search Easy .java

--- LeetCode/Easy/0704-binary-search/0704-binary-search.java
+++ LeetCode/Easy/0704-binary-search/0704-binary-search.java
@@ -3,14 +3,14 @@ class Solution {
         int start=0, end=nums.length-1;
         while(start<=end) {
             int mid=start+(end-start)/2;
-            if(nums[mid]==target) {
-                return mid;
-            }
-            else if(nums[mid]>target) {
+            if(nums[mid]>target) {
                 end=mid-1;
             }
-            else {
+            else if(nums[mid]<target) {
                 start=mid+1;
+            }
+            else {
+                return mid;
             }
         }
         return -1;
