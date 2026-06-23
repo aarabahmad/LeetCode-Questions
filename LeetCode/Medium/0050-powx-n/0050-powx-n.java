a7df3b46e1ada3a3c1b20bdc4fb84bb17e21b2b9
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-06-23 23:53:21 +0530

    0 ms (100%) 47.8 MB (64.49%) 06-23-2026 0050-powx-n Medium .java

--- LeetCode/Medium/0050-powx-n/0050-powx-n.java
+++ LeetCode/Medium/0050-powx-n/0050-powx-n.java
@@ -1,22 +1,19 @@
 class Solution {
-    public double myPow(double x, int n) {
-        long N=n;
-        if(n<0) {
-            return 1/findPow(x, -N);
-        }
-        else {
-            return findPow(x, N);
-        }
-    }
-
     public double findPow(double x, long n) {
         if(n==0) {
             return 1.0;
         }
         double half=findPow(x, n/2);
         double res=half*half;
-        if(n%2==1) {
+        if(n%2!=0) {
             res=res*x;
+        }
+        return res;
+    }
+    public double myPow(double x, int n) {
+        double res=findPow(x, Math.abs(n));
+        if(n<0) {
+            return 1/res;
         }
         return res;
     }
