3e1df997565e98bee2ee90fff55951232c25557d
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-06-24 15:25:17 +0530

    0 ms (100%) 43 MB (98.19%) 06-24-2026 1893-check-if-all-the-integers-in-a-range-are-covered Easy .java

--- LeetCode/Easy/1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.java
+++ LeetCode/Easy/1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.java
@@ -1,17 +1,18 @@
 class Solution {
     public boolean isCovered(int[][] ranges, int left, int right) {
-        int count=0;
+        int[] nums=new int[52];
+        for(int[] arr : ranges) {
+            nums[arr[0]]++;
+            nums[arr[1]+1]--;
+        }
+        for(int i=1;i<nums.length;i++) {
+            nums[i]+=nums[i-1];
+        }
         for(int i=left;i<=right;i++) {
-            boolean res=false;
-            for(int[] nums : ranges) {
-                if(i>=nums[0] && i<=nums[1]) {
-                    res=true;
-                }
-            }
-            if(res==true) {
-                count++;
+            if(nums[i]==0) {
+                return false;
             }
         }
-        return count==(right-left+1);
+        return true;
     }
 }
