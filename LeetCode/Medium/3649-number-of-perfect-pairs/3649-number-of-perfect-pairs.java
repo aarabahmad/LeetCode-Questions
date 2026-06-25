cc9a36b30ffba917963be6f5493b76c35c00252c
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-06-25 14:39:12 +0530

    162 ms (5.56%) 95.9 MB (12.35%) 06-25-2026 3649-number-of-perfect-pairs Medium .java

---
+++ LeetCode/Medium/3649-number-of-perfect-pairs/3649-number-of-perfect-pairs.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public long upperBound(int[] nums, int target) {
+        long start=0, end=nums.length-1;
+        long result=-1;
+        if(target>=nums[(int)end]) {
+            return end+1;
+        }
+        while(start<=end) {
+            long mid=start+(end-start)/2;
+            if(nums[(int)mid]>target) {
+                result=mid;
+                end=mid-1;
+            }
+            else {
+                start=mid+1;
+            }
+        }
+        return result;
+    }
+
+    public long perfectPairs(int[] nums) {
+        for(int i=0;i<nums.length;i++) {
+            nums[i]=Math.abs(nums[i]);
+        }
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+        long res=0;
+        for(int i=0;i<nums.length;i++) {
+            long ub=upperBound(nums, 2*nums[i])-1;
+            res+=(ub-i);
+            // System.out.println(ub+" "+res);
+        }
+        return res;
+    }
+}
