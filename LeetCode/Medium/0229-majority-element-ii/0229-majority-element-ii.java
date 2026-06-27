55cfb8033ae7a7d69a19783f9b736e27f0cb7b7a
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-06-27 18:57:54 +0530

    16 ms (21.63%) 52.4 MB (75.1%) 06-27-2026 0229-majority-element-ii Medium .java

--- LeetCode/Medium/0229-majority-element-ii/0229-majority-element-ii.java
+++ LeetCode/Medium/0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,43 +1,18 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int first=0, second=0;
-        int countFirst=0, countSecond=0;
+        int n=nums.length;
+        Map<Integer, Integer> freq=new HashMap<>();
         for(int num : nums) {
-            if(countFirst==0 && second!=num) {
-                first=num;
-                countFirst=1;
-            }
-            else if(countSecond==0 && first!=num) {
-                second=num;
-                countSecond=1;
-            }
-            else if(first==num) {
-                countFirst++;
-            }
-            else if(second==num) {
-                countSecond++;
-            }
-            else {
-                countFirst--;
-                countSecond--;
-            }
+            freq.put(num, freq.getOrDefault(num, 0)+1);
         }
-        countFirst=0;
-        countSecond=0;
-        for(int num : nums) {
-            if(num==first) {
-                countFirst++;
-            }
-            else if(num==second) {
-                countSecond++;
-            }
-        }
+
         List<Integer> majority=new ArrayList<>();
-        if(countFirst>nums.length/3) {
-            majority.add(first);
-        }
-        if(countSecond>nums.length/3) {
-            majority.add(second);
+        Map<Integer, Integer> added=new HashMap<>();
+        for(int num : nums) {
+            if(freq.get(num)>n/3 && !added.containsKey(num)) {
+                majority.add(num);
+                added.put(num, 0);
+            }
         }
         return majority;
     }
