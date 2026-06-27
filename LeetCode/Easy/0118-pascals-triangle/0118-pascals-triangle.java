ded6f1ecb77f8d5c3559f62c6db672daa09d3453
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-06-27 15:34:04 +0530

    1 ms (93.86%) 43.3 MB (69.49%) 06-27-2026 0118-pascals-triangle Easy .java

--- LeetCode/Easy/0118-pascals-triangle/0118-pascals-triangle.java
+++ LeetCode/Easy/0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,18 +1,18 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> pascal=new ArrayList<>();
+        List<List<Integer>> pattern=new ArrayList<>();
         for(int i=0;i<numRows;i++) {
             List<Integer> row=new ArrayList<>();
             for(int j=0;j<=i;j++) {
-                if((i+j)==i || (i+j)==2*i) {
+                if(j==0 || j==i) {
                     row.add(1);
                 }
                 else {
-                    row.add(pascal.get(i-1).get(j-1)+pascal.get(i-1).get(j));
+                    row.add(pattern.get(i-1).get(j-1)+pattern.get(i-1).get(j));
                 }
             }
-            pascal.add(row);
+            pattern.add(row);
         }
-        return pascal;
+        return pattern;
     }
 }
