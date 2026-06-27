71770272a96fe7ea07d9b8497d37e3d51ccb83e7
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-06-27 13:37:08 +0530

    1 ms (99.98%) 42.3 MB (90.23%) 06-27-2026 0007-reverse-integer Medium .java

--- LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.java
+++ LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.java
@@ -1,16 +1,17 @@
 class Solution {
-    public int reverse(int x) {
-        int sign=(x>=0)?1:-1;
-        long min=Integer.MIN_VALUE;
-        long max=Integer.MAX_VALUE;
-        int rev=0;
-        while(x!=0) {
-            if(((long)rev*10)>max || (long)((long)rev*10)<min) {
+    public int reverse(int n) {
+        int sign=n>=0?1:-1;
+        n=Math.abs(n);
+        int reverse=0;
+        int max=Integer.MAX_VALUE;
+        int min=Integer.MIN_VALUE;
+        while(n!=0) {
+            if(reverse>max/10 || reverse<min/10) {
                 return 0;
             }
-            rev=(rev*10)+(x%10);
-            x/=10;
+            reverse=(reverse*10)+(n%10);
+            n=n/10;
         }
-        return rev;
+        return sign*reverse;
     }
 }
