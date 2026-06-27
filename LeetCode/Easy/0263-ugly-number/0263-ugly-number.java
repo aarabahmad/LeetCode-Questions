43da25742ce6dd9acd848b96da921017fa285ebb
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-06-27 12:55:54 +0530

    1 ms (99.16%) 42.6 MB (32.43%) 06-27-2026 0263-ugly-number Easy .java

--- LeetCode/Easy/0263-ugly-number/0263-ugly-number.java
+++ LeetCode/Easy/0263-ugly-number/0263-ugly-number.java
@@ -3,18 +3,15 @@ class Solution {
         if(n<=0) {
             return false;
         }
-        while(n%5==0) {
+        while(n%5==0 && n>=5) {
             n/=5;
         }
-        while(n%3==0) {
+        while(n%3==0 && n>=3) {
             n/=3;
         }
-        while(n%2==0) {
+        while(n%2==0 && n>=2) {
             n/=2;
         }
-        if(n<=1) {
-            return true;
-        }
-        return false;
+        return n<=1;
     }
 }
