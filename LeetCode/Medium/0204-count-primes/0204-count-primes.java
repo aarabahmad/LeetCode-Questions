14fd3950f6b5d08bf6f09e49da50f7f81f33c698
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-06-27 15:18:41 +0530

    96 ms (73.66%) 51.2 MB (53.14%) 06-27-2026 0204-count-primes Medium .java

--- LeetCode/Medium/0204-count-primes/0204-count-primes.java
+++ LeetCode/Medium/0204-count-primes/0204-count-primes.java
@@ -1,24 +1,27 @@
 class Solution {
-    public void seiveRange(boolean[] seive, int n) {
+    
+    public void buildSeive(boolean[] seive, int num) {
         Arrays.fill(seive, true);
-        for(int i=2;i*i<=n;i++) {
+        seive[0]=false;
+        seive[1]=false;
+        for(int i=2;i*i<=num;i++) {
             if(seive[i]) {
-                for(int j=i*i;j<=n;j+=i) {
+                for(int j=i*i;j<=num;j+=i) {
                     seive[j]=false;
                 }
             }
         }
     }
+
     public int countPrimes(int n) {
-        int prime=0;
-        boolean seive[]=new boolean[n+1];
-        seiveRange(seive, n);
-        for(int i=2;i<n;i++) {
+        boolean[] seive=new boolean[n+2];
+        buildSeive(seive, n);
+        int count=0;
+        for(int i=0;i<n;i++) {
             if(seive[i]) {
-                prime++;
+                count++;
             }
         }
-        return prime;
-
+        return count;
     }
 }
