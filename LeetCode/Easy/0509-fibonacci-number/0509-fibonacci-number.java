78481737fb6c53bb742287e2e64399bf93e6f229
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-06-27 13:58:03 +0530

    0 ms (100%) 42.1 MB (51.47%) 06-27-2026 0509-fibonacci-number Easy .java

--- LeetCode/Easy/0509-fibonacci-number/0509-fibonacci-number.java
+++ LeetCode/Easy/0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,13 +1,17 @@
 class Solution {
     public int fib(int n) {
+        int[] fibonacci=new int[n+1];
+        fibonacci[0]=0;
         if(n==0) {
-            return 0;
+            return fibonacci[n];
         }
-        else if(n==1) {
-            return 1;
+        fibonacci[1]=1;
+        if(n==1) {
+            return fibonacci[n];
         }
-        else {
-            return fib(n-1)+fib(n-2);
+        for(int i=2;i<=n;i++) {
+            fibonacci[i]=fibonacci[i-1]+fibonacci[i-2];
         }
+        return fibonacci[n];
     }
 }
