b8d7cedbc64f645aee321b1eb608e69f111c074d
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-06-27 19:15:28 +0530

    3 ms (72.56%) 52.8 MB (21.14%) 06-27-2026 0229-majority-element-ii Medium .java

--- LeetCode/Medium/0229-majority-element-ii/0229-majority-element-ii.java
+++ LeetCode/Medium/0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,18 +1,45 @@
 class Solution {
+    static int mini=Integer.MIN_VALUE;
     public List<Integer> majorityElement(int[] nums) {
         int n=nums.length;
-        Map<Integer, Integer> freq=new HashMap<>();
+        int majorityOne=mini, countOne=0;
+        int majorityTwo=mini, countTwo=0;
         for(int num : nums) {
-            freq.put(num, freq.getOrDefault(num, 0)+1);
-        }
-
-        List<Integer> majority=new ArrayList<>();
-        Map<Integer, Integer> added=new HashMap<>();
-        for(int num : nums) {
-            if(freq.get(num)>n/3 && !added.containsKey(num)) {
-                majority.add(num);
-                added.put(num, 0);
+            if(majorityOne!=mini && num==majorityOne) {
+                countOne++;
             }
+            else if(majorityTwo!=mini && num==majorityTwo) {
+                countTwo++;
+            }
+            else if(countOne==0) {
+                majorityOne=num;
+                countOne=1;
+            }
+            else if(countTwo==0) {
+                majorityTwo=num;
+                countTwo=1;
+            }
+            else {
+                countOne--;
+                countTwo--;
+            }
+        }
+        countOne=0;
+        countTwo=0;
+        for(int num : nums) {
+            if(num==majorityOne) {
+                countOne++;
+            }
+            else if(num==majorityTwo) {
+                countTwo++;
+            }
+        }
+        List<Integer> majority=new ArrayList<>();
+        if(countOne>n/3) {
+            majority.add(majorityOne);
+        }
+        if(countTwo>n/3) {
+            majority.add(majorityTwo);
         }
         return majority;
     }
