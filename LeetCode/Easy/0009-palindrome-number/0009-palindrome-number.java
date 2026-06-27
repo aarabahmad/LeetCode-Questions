acb77fcdbebfac89bff8355f43c09a01b777fb84
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-06-27 13:07:23 +0530

    5 ms (79.49%) 45.8 MB (86.66%) 06-27-2026 0009-palindrome-number Easy .java

--- LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.java
+++ LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.java
@@ -1,10 +1,19 @@
 class Solution {
-    public boolean isPalindrome(int x) {
-        int rev=0, copy=x;
-        while(copy!=0){
-            rev=(rev*10)+(copy%10);
-            copy/=10;
+    public boolean isPalindrome(int n) {
+        if(n<0) {
+            return false;
         }
-        return (rev==x && x>=0)?true:false;
+        int reverse=0;
+        int max=Integer.MAX_VALUE;
+        int min=Integer.MIN_VALUE;
+        int copy=n;
+        while(copy!=0) {
+            if(reverse>max/10 || reverse<min/10) {
+                return false;
+            }
+            reverse=(reverse*10)+(copy%10);
+            copy=copy/10;
+        }
+        return reverse==n;
     }
 }
