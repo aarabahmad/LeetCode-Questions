aaa904b1fb6628efbc26032762291cee180f5e67
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-06-30 14:52:09 +0530

    2 ms (100%) 84.6 MB (72.45%) 06-30-2026 1109-corporate-flight-bookings Medium .java

--- LeetCode/Medium/1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
+++ LeetCode/Medium/1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
@@ -2,11 +2,14 @@ class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
         int[] res=new int[n];
         for(int[] booking : bookings) {
-            for(int i=booking[0]-1;i<booking[1];i++) {
-                res[i]+=booking[2];
+            res[booking[0]-1]+=booking[2];
+            if(booking[1]!=n) {
+                res[booking[1]]-=booking[2];
             }
         }
-        // System.out.println(Arrays.toString(res));
+        for(int i=1;i<n;i++) {
+            res[i]+=res[i-1];
+        }
         return res;
     }
 }
