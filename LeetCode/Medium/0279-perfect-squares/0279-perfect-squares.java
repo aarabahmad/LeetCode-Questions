36a2152f9582cc5260342a585ba763ae17963b9e
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-07-09 11:42:25 +0530

    22 ms (92.65%) 44 MB (93.24%) 07-09-2026 0279-perfect-squares Medium .java

--- LeetCode/Medium/0279-perfect-squares/0279-perfect-squares.java
+++ LeetCode/Medium/0279-perfect-squares/0279-perfect-squares.java
@@ -1,33 +1,14 @@
 class Solution {
-    public boolean isPerfectSquare(int n) {
-        int sqrt = (int) Math.sqrt(n);
-        return sqrt * sqrt == n;
-    }
-    public int findFourSquares(int n) {
-        int count=0;
-        for (int a = 0; a * a <= n; a++) {
-            for (int b = a; a * a + b * b <= n; b++) {
-                for (int c = b; a * a + b * b + c * c <= n; c++) {
-                    int d2 = n - (a * a + b * b + c * c);
-                    if (isPerfectSquare(d2)) {
-                        int d = (int) Math.sqrt(d2);
-                        if(a!=0) {
-                            count++;
-                        } if(b!=0) {
-                            count++;
-                        } if(c!=0) {
-                            count++;
-                        } if(d!=0) {
-                            count++;
-                        }
-                        return count;
-                    }
-                }
-            }
-        }
-        return count;
-    }
     public int numSquares(int n) {
-        return findFourSquares(n);
+        int[] dp=new int[n+1];
+        dp[0]=0;
+        for(int i=1;i<=n;i++) {
+            int min=Integer.MAX_VALUE;
+            for(int j=1;j*j<=i;j++) {
+                min=Math.min(min, dp[i-j*j]+1);
+            }
+            dp[i]=min;
+        }
+        return dp[n];
     }
 }
