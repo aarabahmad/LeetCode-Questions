cb7352feb6957297026c6164eba5bd95dedf34d8
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-07-12 17:14:36 +0530

    164 ms (24.22%) 306 MB (16.13%) 07-12-2026 1971-find-if-path-exists-in-graph Easy .java

--- LeetCode/Easy/1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ LeetCode/Easy/1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -3,14 +3,11 @@ class Solution {
     Queue<Integer> queue=new LinkedList<>();
     boolean[] visited;
 
-    public boolean bfs(List<List<Integer>> adj, int source, int destination) {
+    public void bfs(List<List<Integer>> adj, int source, int destination) {
         visited[source]=true;
         queue.offer(source);
         while(!queue.isEmpty()) {
             int node=queue.poll();
-            if(node==destination && visited[destination]) {
-                return true;
-            }
             for(int neighbours : adj.get(node)) {
                 if(!visited[neighbours]) {
                     visited[neighbours]=true;
@@ -18,7 +15,15 @@ class Solution {
                 }
             }
         }
-        return false;
+    }
+
+     public void dfs(List<List<Integer>> adj, int source, int destination) {
+        visited[source]=true;
+        for(int neighbours : adj.get(source)) {
+            if(!visited[neighbours]) {
+                dfs(adj, neighbours, destination);
+            }
+        }
     }
 
     public boolean validPath(int n, int[][] edges, int source, int destination) {
@@ -31,7 +36,7 @@ class Solution {
             adj.get(edge[0]).add(edge[1]);
             adj.get(edge[1]).add(edge[0]);
         }
-        boolean res=bfs(adj, source, destination);
-        return res;
+        dfs(adj, source, destination);
+        return visited[destination];
     }
 }
