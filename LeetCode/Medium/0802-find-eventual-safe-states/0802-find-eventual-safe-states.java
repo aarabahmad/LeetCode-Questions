0db7229cc0eeea776b5eccd9ebf0fdb193d66fde
Aarab Ahmad <150062569+aarabahmad@users.noreply.github.com>
2026-07-22 07:41:18 +0530

    4 ms (99.78%) 64.4 MB (16%) 07-22-2026 0802-find-eventual-safe-states Medium .java

--- LeetCode/Medium/0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ LeetCode/Medium/0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -19,15 +19,12 @@ class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int n = graph.length;
         List<Integer> res = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
         boolean[] visited = new boolean[n];
         boolean[] pathVisited = new boolean[n];
-            if (!visited[i]) {
-                boolean cycle=dfs(graph, visited, pathVisited, i);
-                // System.out.println(i+"->"+cycle);
-                if (!cycle) {
-                    res.add(i);
-                }
+        for (int i = 0; i < n; i++) {
+            boolean cycle=dfs(graph, visited, pathVisited, i);
+            if (!cycle) {
+                res.add(i);
             }
         }
         return res;
